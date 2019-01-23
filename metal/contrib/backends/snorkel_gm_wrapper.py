035c047ce96c636c25a32ef4d2302e271aedb64b
Braden Hancock <hancock.braden@gmail.com>
2019-01-22 22:47:46 -0800

    Clean up SnorkelLabelModel

--- metal/contrib/backends/snorkel_gm_wrapper.py
+++ metal/contrib/backends/snorkel_gm_wrapper.py
@@ -1,8 +1,9 @@
 import numpy as np
+from scipy.sparse import csr_matrix, issparse
 from snorkel.learning.gen_learning import GenerativeModel
 
 from metal.label_model import LabelModel
-from metal.utils import recursive_merge_dicts
+from metal.utils import categorical_to_plusminus, recursive_merge_dicts
 
 
 class SnorkelLabelModel(LabelModel):
@@ -12,18 +13,16 @@ class SnorkelLabelModel(LabelModel):
         super().__init__()
         self.model = GenerativeModel()
 
-    @staticmethod
-    def categorical_to_plusminus(L):
-        L = L.copy()
-        L[L == 2] = -1
-        return L
-
     def train_model(self, L, **kwargs):
-        L = self.categorical_to_plusminus(L)
+        L = categorical_to_plusminus(L)
+        if not issparse(L):
+            L = csr_matrix(L)
         self.model.train(L, verbose=False, **kwargs)
 
     def predict_proba(self, L):
-        L = self.categorical_to_plusminus(L)
+        L = categorical_to_plusminus(L)
+        if not issparse(L):
+            L = csr_matrix(L)
         marginals = self.model.marginals(L).reshape(-1, 1)
         Y_ps = np.hstack((marginals, 1 - marginals))
         return Y_ps
