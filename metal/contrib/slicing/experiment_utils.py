bfd37df93aa3adaf45a87c380fb9ec939fb10924
Vincent Chen <vincentschen@yahoo.com>
2019-01-24 01:12:06 -0800

    refactor dataloaders

--- metal/contrib/slicing/experiment_utils.py
+++ metal/contrib/slicing/experiment_utils.py
@@ -1,63 +1,64 @@
+import numpy as np
 import torch
+from scipy.sparse import csr_matrix
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from scipy.sparse import csr_matrix
 
+from metal.contrib.slicing.online_dp import SliceHatModel
 from metal.contrib.slicing.utils import generate_weak_labels
 from metal.end_model import EndModel
-from metal.contrib.slicing.online_dp import SliceHatModel
 from metal.utils import SlicingDataset
 
-def create_data_loaders(Ls, Xs, Ys, Zs, model_config):
+
+def create_data_loader(Ls, Xs, Ys, Zs, model_config, split, L_weights=None):
     """
-    Creates train, dev, and test dataloaders based on raw input data and config.
+    Creates train, dev, or test dataloaders based on raw input data and config.
 
     Returns:
         (train_dl, dev_dl, test_dl)
     """
-    
-    L_weights = model_config.get("L_weights")
-    assert(isinstance(L_weights, list) or L_weights is None)
 
-    is_slicing = "slice_kwargs" in model_config.keys()
-    
-    # Generate weak labels:
-    # a) uniform (L_weights = [1,...,1])
-    # b) manual  (L_weights = [1,X,...1])
-    # c) learned (L_weights = None): DP
-    L_train = Ls[0].toarray() if isinstance(Ls[0], csr_matrix) else Ls[0]
-    Y_weak = generate_weak_labels(L_train, L_weights)
+    assert split in ["train", "dev", "test"]
+    assert isinstance(L_weights, list) or L_weights is None
 
-    if is_slicing:
-        train_dataset = SlicingDataset(Xs[0], L_train, Y_weak)
-    else:
-        train_dataset = SlicingDataset(Xs[0], Y_weak)
+    if split == "train":
+        is_slicing = "slice_kwargs" in model_config.keys()
 
-    dev_dataset = SlicingDataset(Xs[1], Ys[1])
-    test_dataset = SlicingDataset(Xs[2], Ys[2], Zs[2])
+        # Generate weak labels:
+        # a) uniform (L_weights = [1,...,1])
+        # b) manual  (L_weights = [1,X,...1])
+        # c) learned (L_weights = None): DP
+        L_train = Ls[0].toarray() if isinstance(Ls[0], csr_matrix) else Ls[0]
+        Y_weak = generate_weak_labels(L_train, L_weights)
+
+        dataset = (
+            SlicingDataset(Xs[0], L_train, Y_weak)
+            if is_slicing
+            else SlicingDataset(Xs[0], Y_weak)
+        )
+        shuffle = True
+
+    elif split == "dev":
+        dataset = SlicingDataset(Xs[1], Ys[1])
+        shuffle = False
+
+    elif split == "test":
+        dataset = SlicingDataset(Xs[2], Ys[2], Zs[2])
+        shuffle = False
 
     batch_size = model_config.get("train_kwargs", {}).get("batch_size", 32)
-    return (
-        DataLoader(train_dataset, batch_size=batch_size, shuffle=True), 
-        DataLoader(dev_dataset, batch_size=batch_size, shuffle=False), 
-        DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-    )
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
 
-def train_model(
-    config,
-    train_loader,
-    dev_loader,
-    m,
-    verbose=False):
+def train_model(config, Ls, Xs, Ys, Zs, m):
     """
     Generates weak labels and trains a single model
 
     Args:
         config: model config with "end_model_init_kwargs"
             optional: "train_kwargs", "slice_kwargs" (if slice model)
-        train_loader: data loader for train data 
-            if slice: (X, L, Y) 
+        train_loader: data loader for train data
+            if slice: (X, L, Y)
             else: (X, Y)
         dev_loader: dataloader for validation data (X, Y)
         m: num labeling sources
@@ -70,9 +71,13 @@ def train_model(
     model = EndModel(**config["end_model_init_kwargs"])
 
     # Add slice hat if applicable
-    slice_kwargs = config.get('slice_kwargs')
+    slice_kwargs = config.get("slice_kwargs")
     if slice_kwargs:
         model = SliceHatModel(model, m, **slice_kwargs)
+
+    # Create data loaders
+    train_loader = create_data_loader(Ls, Xs, Ys, Zs, config, "train")
+    dev_loader = create_data_loader(Ls, Xs, Ys, Zs, config, "dev")
 
     # train model
     train_kwargs = config.get("train_kwargs", {})
@@ -149,3 +154,16 @@ def separate_eval_loader(data_loader):
     X = torch.cat(X)
     Y = torch.cat(Y)
     return X, Y, Z
+
+
+# def search_upweighting_models(
+# config, train_loader, dev_loader, m, targeting_lfs_idx
+# ):
+
+# for multiplier in [2, 3]:
+#     L_weights = np.ones(m)
+#     L_weights[targeting_lfs_idx] = multiplier
+#     L_weights = list(L_weights)
+#     train_ds, dev_ds, _ = create_data_loaders(
+#         Ls, Xs, Ys, Zs, config, L_weights
+#     )
