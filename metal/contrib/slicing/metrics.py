f931aaf3a201f40a005ac1e95f3b99403b60f11a
Braden Hancock <hancock.braden@gmail.com>
2019-02-01 14:08:50 -1000

    Clean up custom slicing metrics

--- metal/contrib/slicing/metrics.py
+++ metal/contrib/slicing/metrics.py
@@ -1,7 +1,9 @@
 # Define custom metrics
 import numpy as np
+
 from metal.metrics import accuracy_score
 from metal.utils import convert_labels
+
 
 def calc_heads_acc(model, dataloader):
     L, X, Y, Z = dataloader.dataset.data
@@ -11,34 +13,38 @@ def calc_heads_acc(model, dataloader):
         L = L.clone()
         L[L == 2] = 1
     else:
-        raise NotImplemented("Cannot evaluate L predictions if model.indicator=False")
-    
-    Y_off_preds = convert_labels(np.round(Y_off_probs[:, 0]), "onezero", "categorical")
-    Y_on_preds = convert_labels(np.round(Y_on_probs[:, 0]), "onezero", "categorical")
-    metrics = {}    
-    
+        raise NotImplementedError(
+            "Cannot evaluate L predictions if model.indicator=False"
+        )
+
+    Y_off_preds = convert_labels(
+        np.round(Y_off_probs[:, 0]), "onezero", "categorical"
+    )
+    Y_on_preds = convert_labels(
+        np.round(Y_on_probs[:, 0]), "onezero", "categorical"
+    )
+    metrics = {}
+
     metrics = {
         "L_acc": accuracy_score(L.flatten(), np.round(L_probs).flatten()),
         "Y_off_acc": accuracy_score(Y, Y_off_preds),
-        "Y_on_acc": accuracy_score(Y, Y_on_preds),     
+        "Y_on_acc": accuracy_score(Y, Y_on_preds),
     }
-    
+
     return metrics
+
 
 def calc_slice_acc(model, dataloader):
     model.warn_once("Slice accuracies currently only work for valid set!")
     L, X, Y, Z = dataloader.dataset.data
-    Y_preds = model.predict(X)
 
     metrics = {}
     slices = sorted(list(np.unique(Z)))
-    slices.remove(0) # remove "background" points
+    slices.remove(0)  # remove "background" points
     for s in slices:
         inds = [i for i, e in enumerate(Z) if e == s]
         X_slice = X[inds]
         Y_slice = Y[inds]
-        Y_preds_slice = Y_preds[inds]
         acc = model.score((None, X_slice, Y_slice, None), verbose=False)
         metrics[f"slice_{s}"] = acc
     return metrics
-
