bb1daea530b1d446c081adce7d9709735f51df43
jdunnmon <jdunnmon@gmail.com>
2019-04-15 21:11:59 -0700

    fixed neg-pos issue in cxr_slices

--- metal/mmtl/cxr/cxr_slices.py
+++ metal/mmtl/cxr/cxr_slices.py
@@ -4,7 +4,7 @@ import torch
 from torch.utils.data import Dataset
 
 def chest_drain_cnn_neg(dataset: Dataset) -> dict:
-    data_file = os.path.join(os.environ["CXRDATA"],'CXR8-DRAIN-SLICE-POS',f"{dataset.split}.tsv")
+    data_file = os.path.join(os.environ["CXRDATA"],'CXR8-DRAIN-SLICE-NEG',f"{dataset.split}.tsv")
     slice_data = pd.read_csv(data_file, sep='\t')
     keys = slice_data['data_index'].tolist()
     values = [int(not l) for l in slice_data['slice_label'].astype(int)]
