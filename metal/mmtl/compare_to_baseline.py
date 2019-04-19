07f8b688fe622321c3aabbb2aed1d25d0a1a89c1
Zhenzhen Weng <zhenzhenweng92@gmail.com>
2019-04-19 15:57:01 -0700

    address comments

--- metal/mmtl/compare_to_baseline.py
+++ metal/mmtl/compare_to_baseline.py
@@ -14,7 +14,7 @@ from metal.mmtl.trainer import MultitaskTrainer, trainer_defaults
 python compare_to_baseline.py --seed 1 --tasks RTE --slice_dict '{"RTE": ["dash_semicolon", "more_people", "BASE"]}'
 """
 
-def get_baseline_scores(tasks, seed, eval_payload, slice_dict):
+def get_baseline_scores(tasks, seed, eval_payload):
     # Baseline Model
     task_name = tasks[0].name
     model = MetalModel(tasks, seed=seed, verbose=False)
@@ -22,7 +22,7 @@ def get_baseline_scores(tasks, seed, eval_payload, slice_dict):
     trainer.train_model(
         model,
         payloads,
-        checkpoint_metric="{}/{}_valid/{}_gold/accuracy".format(task_name, task_name, task_name),
+        checkpoint_metric="{}/{}/{}_gold/accuracy".format(task_name, eval_payload.name, task_name),
         checkpoint_metric_mode="max",
         checkoint_best=True,
         writer="tensorboard",
@@ -36,30 +36,25 @@ def get_baseline_scores(tasks, seed, eval_payload, slice_dict):
         checkpoint_best=True,
         checkpoint_cleanup=False,
     )
-    # NOTE: we need to retarget slices to the original RTE head
-    for label_name in slice_dict[task_name].values():
-        label_name = '{}_slice:'.format(task_name) + label_name
-        eval_payload.retarget_labelset(label_name, task_name)
     return model.score(eval_payload)
 
 
-def get_model_scores(tasks_slice, payloads_slice, seed, eval_payload, slice_dict):
+def get_model_scores(tasks_slice, payloads_slice, seed, eval_payload):
     task_name = tasks_slice[0].name
     slicing_tasks = convert_to_slicing_tasks(tasks_slice)
     slice_model = SliceModel(slicing_tasks, seed=seed, verbose=False)
     task_metrics = []
     prefix = '{}/{}_'.format(task_name, task_name)
     for p_name, metric in [('train', 'loss'), ('valid', 'accuracy')]:
-        task_metrics += [prefix+p_name+'/{}_gold/{}'.format(task_name, metric)]
-        for label_name in slice_dict[task_name].values():
-            task_metrics += [prefix + p_name + '/{}_slice:{}/{}'.format(task_name, label_name, metric)]
+        for label_name in eval_payload.labels_to_tasks.keys():
+            task_metrics += [prefix + p_name + '/{}/{}'.format(task_name, label_name, metric)]
     print(task_metrics)
     trainer = MultitaskTrainer(seed=seed)
     trainer.train_model(
         slice_model,
         payloads_slice,
         task_metrics=task_metrics,
-        checkpoint_metric="{}/{}_valid/{}_gold/accuracy".format(task_name, task_name, task_name),
+        checkpoint_metric="{}/{}/{}_gold/accuracy".format(task_name, eval_payload.name, task_name),
         checkpoint_metric_mode="max",
         checkoint_best=True,
         writer="tensorboard",
@@ -101,37 +96,39 @@ if __name__ == "__main__":
     model_config = recursive_merge_dicts(model_defaults, vars(args), misses="ignore")
     task_config = recursive_merge_dicts(task_defaults, vars(args), misses="ignore")
     args = parser.parse_args()
-    task_kwargs = {
-        "dl_kwargs": {"batch_size": 16},
-        "freeze_bert": False,
-        "bert_model": 'bert-base-cased',
-        "max_len": 200,
-        "attention": False
-    }
-    task_names = args.tasks.split(',')  # ['RTE']
+
+    task_names = args.tasks.split(',')
     assert len(task_names) == 1
+    task_name = task_names[0]
 
     # Create tasks and payloads
-    tasks, payloads = create_glue_tasks_payloads(task_names, **task_kwargs)
+    task_config["slice_dict"] = None
+    task_config['attention'] = False
+    tasks, payloads = create_glue_tasks_payloads(task_names, **task_config)
+
+    # Create slicing tasks and payloads
     slice_dict = json.loads(args.slice_dict)
-    task_kwargs.update({"slice_dict": slice_dict})
-    task_kwargs['attention'] = None
+    task_config.update({"slice_dict": slice_dict})
+    task_config['attention'] = None
     tasks_slice, payloads_slice = create_glue_tasks_payloads(
-        task_names, **task_kwargs
+        task_names, **task_config
     )
 
     eval_payload = copy.deepcopy(payloads_slice[1])
 
+    # NOTE: we need to retarget slices to the original RTE head
+    for label_name in [t.name for t in tasks_slice[1:]]:
+        eval_payload.retarget_labelset(label_name, task_name)
+
     if args.model_type.upper() == 'METAL':
         baseline_scores = get_baseline_scores(
-            tasks, args.seed, eval_payload, slice_dict
+            tasks, args.seed, eval_payload
         )
     else:
-        raise Exception('Not Yet Implemented!')
+        raise NotImplementedError
 
-    eval_payload = copy.deepcopy(payloads_slice[1])
     model_scores = get_model_scores(
-        tasks_slice, payloads_slice, args.seed, eval_payload, slice_dict
+        tasks_slice, payloads_slice, args.seed, eval_payload
     )
     print(baseline_scores)
     print(model_scores)
