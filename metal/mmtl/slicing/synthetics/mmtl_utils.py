34da598baefcd579d620c907c5f956b58db750ab
Vincent Chen <vincentschen@yahoo.com>
2019-04-27 01:34:56 -0700

    Add mixture of experts synthetics; Remove loss_multiplier defaults from synthetics

--- metal/mmtl/slicing/synthetics/mmtl_utils.py
+++ metal/mmtl/slicing/synthetics/mmtl_utils.py
@@ -8,6 +8,7 @@ from metal.mmtl.slicing.synthetics.data_generator import (
     generate_slice_labels,
 )
 from metal.mmtl.slicing.tasks import BinaryClassificationTask, create_slice_task
+from metal.mmtl.trainer import MultitaskTrainer
 
 
 def remap_labelsets(payload, labels_to_tasks):
@@ -28,33 +29,46 @@ def remap_labelsets(payload, labels_to_tasks):
             payload.retarget_labelset(label_name, None)
 
 
-def create_tasks(task_name, slice_names=[], verbose=False):
+def create_tasks(
+    task_name,
+    slice_names=[],
+    slice_weights={},
+    create_ind=True,
+    create_base=True,
+    verbose=False,
+):
     input_module = nn.Sequential(nn.Linear(2, 5), nn.ReLU())
     head_module = nn.Linear(5, 1)  # NOTE: slice_model requires 1dim output head
     task = BinaryClassificationTask(
         name=task_name, input_module=input_module, head_module=head_module
     )
-    tasks = [task]
+    tasks = [task] if create_base else []
 
     # for each slice create an 'ind' task: predicts whether we are in the slice
     # and a 'pred' task: "expert" on predicting labels of slice
     for slice_name in slice_names:
         # TODO: make loss_multiplier a parameter
-        loss_multiplier = 1.0 / (2 * len(slice_names))
+        # loss_multiplier = 1.0 / (2 * len(slice_names))
+        if slice_name in slice_weights:
+            loss_multiplier = slice_weights[slice_name]
+        else:
+            loss_multiplier = 1.0
         slice_pred_task = create_slice_task(
             task,
             f"{task_name}:{slice_name}:pred",
             "pred",
             loss_multiplier=loss_multiplier,
         )
-        slice_ind_task = create_slice_task(
-            task,
-            f"{task_name}:{slice_name}:ind",
-            "ind",
-            loss_multiplier=loss_multiplier,
-        )
         tasks.append(slice_pred_task)
-        tasks.append(slice_ind_task)
+
+        if create_ind:
+            slice_ind_task = create_slice_task(
+                task,
+                f"{task_name}:{slice_name}:ind",
+                "ind",
+                loss_multiplier=loss_multiplier,
+            )
+            tasks.append(slice_ind_task)
 
     if verbose:
         print(f"Creating {len(tasks)} tasks...")
@@ -71,20 +85,22 @@ def create_payloads(
     batch_size=1,
     slice_funcs={},
     SPLITS=["train", "valid", "test"],
+    create_ind=True,
+    create_base=True,
     verbose=False,
 ):
 
     payloads = []
-    labels_to_tasks = {"labelset_gold": task_name}
+    labels_to_tasks = {"labelset_gold": task_name} if create_base else {}
     for i, split in enumerate(SPLITS):
         payload_name = f"payload_{split}"
 
         # convert to torch tensors
         X_dict = {"data": torch.Tensor(Xs[i]), "uids": torch.Tensor(uid_lists[i])}
-        Y_dict = {"labelset_gold": torch.Tensor(Ys[i])}
+        Y_dict = {"labelset_gold": torch.Tensor(Ys[i])} if create_base else {}
 
         if slice_funcs:
-            slice_labels = generate_slice_labels(Xs[i], Ys[i], slice_funcs)
+            slice_labels = generate_slice_labels(Xs[i], Ys[i], slice_funcs, create_ind)
 
             # labelset_name -> {"ind": [1,2,1,2,2], "pred": [0,1,0,2,2]}
             for slice_name, slice_label in slice_labels.items():
@@ -105,3 +121,44 @@ def create_payloads(
         for p in payloads:
             print(p)
     return payloads
+
+
+def train_slice_experts(
+    uid_lists, Xs, Ys, model_class, slice_funcs, batch_size=16, **trainer_kwargs
+):
+    """ For each slice_func,
+        1) initializes a model using model_class(tasks),
+        2) trains on payloads where data is masked by slice.
+    """
+
+    experts = {}
+    task_name = "expert"
+    for slice_name, slice_fn in slice_funcs.items():
+        tasks = create_tasks(
+            task_name,
+            slice_names=[slice_name],
+            create_ind=False,
+            create_base=False,
+            verbose=True,
+        )
+        payloads = create_payloads(
+            task_name,
+            uid_lists,
+            Xs,
+            Ys,
+            batch_size=batch_size,
+            slice_funcs={slice_name: slice_fn},
+            create_ind=False,
+            create_base=False,
+            verbose=True,
+        )
+        print(tasks)
+        print(payloads)
+        model = model_class(tasks)
+        seed = trainer_kwargs.get("seed", None)
+        trainer = MultitaskTrainer(seed=seed)
+        metrics_dict = trainer.train_model(model, payloads, **trainer_kwargs)
+        print(metrics_dict)
+        experts[slice_name] = model
+
+    return experts
